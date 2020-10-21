cea90c96e252a68494a8cfba5875523527a6b953
Bryson <brysonmunroe@gmail.com>
2020-10-21 16:48:12 -0500

    worked on push

--- src/SuperStackTest.java
+++ src/SuperStackTest.java
@@ -1,4 +1,37 @@
 
+
+import static org.junit.Assert.fail;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
 public class SuperStackTest {
+	
+	@BeforeEach
+	
+	@Test
+	void testSuperStack() {
+		fail("Not yet implemented");
+	}
+	
+	@Test 
+	void testIsEmpty() {
+		SuperStack<Integer> stack = new SuperStack<Integer>();
+		
+	}
+	@test
+	void testIsFull() {
+		
+	}
+	@test
+	void testPush() {
+		
+	}
+	@test
+	void testPop() {
+		
+	}
+	
+	
 
 }
